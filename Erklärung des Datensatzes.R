22e870bab008e1c6b3be527ce02ed02b84df45da
smaier1509 <78968264+smaier1509@users.noreply.github.com>
2021-02-17 14:28:13 +0100

    Add files via upload
    Erklärung des Datensatzes durch Tim Schmale und Stefan Maier.

---
+++ Erklärung des Datensatzes.R
@@ -0,0 +1,29 @@
+### Zusammenfassung der erklaerten Wahrscheinlichkeiten für den Datensatz:
+
+# Bei der Zusammenstellung des Datensatzes haben wir bei den 
+# Wahrscheinlichkeiten fuer das Interesse an Mathematik (1 = sehr niedrig, 7 = 
+# sehr hoch) eine Abhaengigkeit zum Studiengang eingebracht. 
+# Das Interesse ist beim Studiengang Statistik relativ hoch, mit den hoechsten
+# Wahrscheinlichkeiten im oberen Bereich des Intervalls. Beim Studiengang 
+# Data Science ist das Interesse auch noch relativ hoch, aber schon niedriger
+# d.h. die hoechsten Wahrscheinlichkeiten sind bei niedrigeren Werten. Beim 
+# Studiengang Mathematik sind die Wahrscheinlichkeiten bei den hoechsten Werten,
+# was fuer uns eine intuitive Anordnung ist. Beim Studiengang Informatik 
+# sind die Wahrscheinlichkeiten bei den niedrigen Werten am hoechsten. Da
+# Informatiker nicht allzu sehr an der Mathematik interessiert sind, sondern 
+# eher am Programmieren.
+#
+# Die Wahrscheinlichkeiten fuer die Werte vom Interesse an Programmieren sind 
+# einmal entgegengesetzt aufgebaut. Somit sind die Werte fuer den Studiengang 
+# Mathematik am hoechsten bei den niedrigen Werten im Intervall. Bei den 
+# Studiengaengen Statistik und Data Science sind die Wahrscheinlichkeiten am 
+# hoechsten in den oberen Bereichen, wobei die aber bei Data Science etwas 
+# hoeher als bei Statistik. Beim Studiengang Informatik sind die 
+# Wahrscheinlichkeiten auch intuitiv am hoechsten bei den hoechsten Werten im 
+# Intervall. 
+#
+# Beim belegten Mathe-LK haben wir die Wahrscheinlichkeiten an ein hohes 
+# Interesse an Mathematik geknuepft, weshalb die Wahrscheinlichkeiten auch 
+# beim Studiengang Mathematik am hoechsten ist. Absteigend darauf folgen 
+# Statistik, Data Science und mit hohem Abstand danach Informatik.
+#
