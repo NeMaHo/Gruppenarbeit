9650daee890231b03f1fd61b3d116662d81f24f1
NeMaHo <78764590+NeMaHo@users.noreply.github.com>
2021-02-21 03:51:11 +0100

    Update Funktionen2.R

--- Funktionen2.R
+++ Funktionen2.R
@@ -26,3 +26,19 @@ category <- function(x, value) {
   
 }
 
+
+# Hilfsfunktion zu plotCategorical: nominalH
+# -> Bestimmt Tabellen mit absoluten Haeufigkeiten fuer nominalskalierte Variablen
+
+# Input:
+# - x: Vektor
+
+# Output: Tabelle mit absoluten Haeufigkeiten, wenn Vektor nominal, sonst NULL
+
+nominalH <- function(x)
+{
+  if(is.character(x))
+  {
+    table(factor(x, levels = levels(as.factor(x))))
+  }
+}
